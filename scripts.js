03cdf8c56e5c62fa118268b4a30ef95cf6634094
Jordan Blount <jordantblount@gmail.com>
2021-05-03 15:25:54 -0400

    Initial commit. Basic logic

---
+++ scripts.js
@@ -0,0 +1,10 @@
+//TODO: Add multiply, divide, subtract, add. (Look up the rules for each)
+//      Do not forget about negative numbers.
+
+//TODO: Add functionality to the plus/negative button
+
+//TODO: Method to display text
+
+//TODO: Add percents using floats (I think). I'll probably have to convert
+//      between floats and integers. Look that up.
+
