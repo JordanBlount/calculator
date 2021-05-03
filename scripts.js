03b4c310558b8aad444032bdd92e57664e08364d
Jordan Blount <jordantblount@gmail.com>
2021-05-03 16:54:52 -0400

    Add more CSS styles and started basic logic in Javascript.

--- scripts.js
+++ scripts.js
@@ -8,3 +8,24 @@
 //TODO: Add percents using floats (I think). I'll probably have to convert
 //      between floats and integers. Look that up.
 
+//TODO: Make it so that a button's focus can be maintained. OR change all
+//      to divs instead of buttons. EX: the symbol buttons
+
+//TODO: Make current_number text move to left dynamically based 
+//      on size of number
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+function add (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
