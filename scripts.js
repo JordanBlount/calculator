38900f23be960d9523568349962423f74f5d93e1
Jordan Blount <jordantblount@gmail.com>
2021-05-03 21:31:50 -0400

    Added a lot of logic

--- scripts.js
+++ scripts.js
@@ -14,12 +14,85 @@
 //TODO: Make current_number text move to left dynamically based 
 //      on size of number
 
+const display = document.getElementById('current_number');
+const clearButton = document.querySelector('#clear');
+const equalsButton = document.querySelector('#equals');
+
+let firstNum = "";
+let secondNum = "";
+let currentOperator = null;
+let resetScreen = false;
+
+const numbers = Array.from(document.querySelectorAll('.number'));
+numbers.forEach(num => {
+    num.addEventListener('click', function() {
+        if(display.textContent == "Not a number") {
+            display.textContent = "";
+        }
+        appendNum(num.textContent);
+    });
+});
+const symbolButtons = Array.from(document.querySelectorAll('.symbol_button'));
+symbolButtons.forEach(symbol => {
+    symbol.addEventListener('click', function() {
+        console.log("REACHED");
+        setOperation(symbol.textContent);
+    });
+});
+
+equalsButton.addEventListener('click', calculate);
+clearButton.addEventListener('click', clearScreen);
+
+function appendNum(num) {
+    if (display.textContent === '0' || resetScreen) reset();
+    display.textContent += num;
+}
+
+function setOperation(operation) {
+    if(currentOperator !== null) calculate();
+    firstNum = display.textContent;
+    currentOperator = operation;
+    resetScreen = true;
+}
+
+function calculate() {
+    if(currentOperator === null || resetScreen) return;
+    if(currentOperator === '÷' && display.textContent === '0') {
+        display.textContent = "Not a number";
+        clearScreen();
+        return;
+    }
+    secondNum = display.textContent;
+    console.log('CHEGOU');
+    display.textContent = roundNum(operate(currentOperator, firstNum, secondNum));
+    console.log(display.textContent);
+    currentOperator = null;
+}
+
+function roundNum(num) {
+    return Math.round(num * 100) / 100;
+}
+
+function reset() {
+    display.textContent = "";
+    resetScreen = false;
+}
+
+function clearScreen() {
+    display.textContent = '0';
+    firstNum = '';
+    secondNum = '';
+    currentOperator = null;
+    resetScreen = false;
+}
+
 function multiply (a, b) {
     return a * b;
 }
 
 function divide (a, b) {
-    return a / b;
+    if(b === 0) return null;
+        else return a / b;
 }
 
 function add (a, b) {
@@ -28,4 +101,20 @@ function add (a, b) {
 
 function subtract (a, b) {
     return a - b;
+}
+
+function operate (operator, a, b) {
+    switch(operate) {
+        case '*':
+            return multiply(a, b);;
+        case '÷':
+            return divide(a, b);
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        default:
+            return null;
+    }
+    return value;
 }
